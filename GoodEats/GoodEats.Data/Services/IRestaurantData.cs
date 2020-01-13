87a150af40ee9c072eb1f3f08512795d325f6bc1
Gudbrandr42 <mark.bullard42@gmail.com>
2020-01-13 16:36:00 -0500

    added in-memory data source as part of following tutorial

---
+++ GoodEats/GoodEats.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using GoodEats.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodEats.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
